3d3ee3d0656985655be3e2fddc4f4bdfbee63f2f
y-shoraka <shorakayasaman@gmail.com>
2023-02-24 19:05:59 +0330

    update the package.json

--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,29 @@
-import { useState } from "react";
+import React,{ useState } from "react";
 import "./Weathers.css";
 import axios from "axios";
 
-export default function Weather(props) {
+export default function Weather() {
   let [City, SetCity] = useState("");
+  let [temprature, setTemprature] = useState("");
   function ShowResult(response) {
-    console.log(response)
+    console.log(response.data);
+    setTemprature(
+      <div>
+        <li>Tempreture : {Math.round(response.data.main.temp)}°C </li>
+        <li>Humidity : {response.data.main.humidity}%</li>
+        <li>Wind : {Math.round(response.data.wind.speed)} Km/hr</li>
+        <li>Feels like : {Math.round(response.data.main.feels_like)}°C </li>
+      </div>
+    );
   }
-  function getResult() {
-    apiKey = "5122c73584c58f76c2670a7ce4d20f3a";
+  function getResult(event) {
+    event.preventDefault();
+
+    let apiKey = "5122c73584c58f76c2670a7ce4d20f3a";
+    console.log(City.target.value);
     axios
       .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+        `https://api.openweathermap.org/data/2.5/weather?q=${City.target.value}&appid=${apiKey}&units=metric`
       )
       .then(ShowResult);
   }
@@ -22,6 +34,7 @@ export default function Weather(props) {
         <input type="search" placeholder="Search for City" onChange={SetCity} />
         <input className="submit" type="submit" value="Search" />
       </form>
+      {temprature}
     </div>
   );
 }
