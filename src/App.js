729360dec30df490c34ac0bc85ca612a4d80364c
y-shoraka <shorakayasaman@gmail.com>
2023-02-24 19:16:36 +0330

    Added Discription and github sourcer at footer

--- src/App.js
+++ src/App.js
@@ -6,6 +6,18 @@ export default function App() {
     <div className="App">
       <header className="App-header">
         <Weather />
+        <p>
+          <a
+            href="https://github.com/y-shoraka/react-weather"
+            target={"_blank"}
+          >
+            Open-Source Code
+          </a>{" "}
+          , by Yasaman Shoraka from{" "}
+          <a href="https://www.shecodes.io/" target={"_blank"}>
+            SheCodes
+          </a>
+        </p>
       </header>
     </div>
   );
