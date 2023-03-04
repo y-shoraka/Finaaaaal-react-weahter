96d2241b721aa522e02efa019fd5c381f6a19c24
y-shoraka <shorakayasaman@gmail.com>
2023-03-04 13:41:05 +0330

    passing the data from waether to chart

--- src/Chart.js
+++ src/Chart.js
@@ -1,57 +1,72 @@
-import React, { Component } from 'react';
-import CanvasJSReact from './canvasjs.react';
-var CanvasJSChart = CanvasJSReact.CanvasJSChart; 
+import React, { Component } from "react";
+import CanvasJSReact from "./canvasjs.react";
+import axios from "axios";
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+let Humidity =[] ;
 
-/*function humidity(response){
-console.log(response.data);
-}
-firstData();
-function firstData(){
-        axios
-        .get(
-          `https://api.shecodes.io/weather/v1/forecast?query=london&key=d622ab03edofbbtc80f362a442d6777c&units=metric`
-        )
-        .then(humidity); 
-    }*/
 class Chart extends Component {
-   
-	render() {
-		const options = {
-            backgroundColor: "#101016" ,
-            animationEnabled: true,
-            dataPointWidth: 10 ,
-            height: 200, 
-            width:200,
-            margin: 10 ,
-            color:"grey" ,
-			title: {
-                fontColor: "white",
-				text: "Humidity"
-			},
-			data: [
-                
-			{
-                color:"#BBD8EC",
-				// Change type to "doughnut", "line", "splineArea", etc.
-				type: "column",
-				dataPoints: [
-					{ label: "Apple",  y: 10  },
-					{ label: "Orange", y: 15  },
-					{ label: "Banana", y: 25  },
-					{ label: "Mango",  y: 30  },
-					{ label: "Grape",  y: 28  }
-				]
-			}
-			]
-		}
-		return (
-		<div>
-			<CanvasJSChart options = {options}
-				/* onRef={ref => this.chart = ref} */
-			/>
-			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-		</div>
-		);
-	}
+  render() {
+	function humidity(response) {
+		return Humidity = [
+		  response.data.daily[0].temperature.humidity,
+		  response.data.daily[1].temperature.humidity,
+		  response.data.daily[2].temperature.humidity,
+		  response.data.daily[3].temperature.humidity,
+		  response.data.daily[4].temperature.humidity,
+		  response.data.daily[5].temperature.humidity,
+		  response.data.daily[6].temperature.humidity,
+		 
+		];
+	  }
+	  axios
+		.get(
+		  `https://api.shecodes.io/weather/v1/forecast?query=${this.props}&key=d622ab03edofbbtc80f362a442d6777c&units=metric`
+		)
+		.then(humidity);
+		
+    let options = {
+ 
+      backgroundColor: "#101016",
+      animationEnabled: true,
+      dataPointWidth: 10,
+      height: 200,
+      width: 200,
+      margin: 10,
+      color: "grey",
+      title: {
+        fontColor: "white",
+        text: this.props.props,
+        fontFamily: "sans-serif",
+      },
+      data: [
+        {
+			
+          color: "#BBD8EC",
+          // Change type to "doughnut", "line", "splineArea", etc.
+          type: "column",
+          dataPoints: [
+            { label: "day1", y: Humidity[0] },
+            { label: "day2", y: Humidity[1] },
+            { label: "day3", y: Humidity[2] },
+            { label: "day4", y: Humidity[3] },
+            { label: "day5", y: Humidity[4] },
+			{ label: "day5", y: Humidity[5] },
+			{ label: "day5", y: Humidity[6] },
+
+          ],
+        },
+      ],
+    };
+    return (
+      <div>
+		<div>{this.props.forecastCity}</div>
+        <CanvasJSChart
+          options={options} props = {this.props.forecastCity}
+          /* onRef={ref => this.chart = ref} */
+        />
+        {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+      </div>
+    );
+  }
 }
-export default Chart ;                             
+export default Chart;
